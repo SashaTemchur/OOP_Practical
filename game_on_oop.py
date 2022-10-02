8d2281c9baa56fa420ca966b5e8e34e7f0853fc8
sasha <alextemchur7@gmail.com>
2022-10-02 22:11:22 +0300

    Game

--- game_on_oop.py
+++ game_on_oop.py
@@ -8,6 +8,7 @@ enemy_armor = int(input(" Enemy Armor:"))
 
 
 class Person:
+    # Creates a main class that specifies common parameters for the player and the enemy
     def __init__(self, health, damage, armor):
         self.health = health
         self.damage = damage
@@ -15,6 +16,7 @@ class Person:
 
 
 class Player:
+    # Creates a class that inherits from the main class, this class describes the player's health and damage armor
     def __init__(self, health, damage, armor):
         Person.__init__(self, health, damage, armor)
 
@@ -39,6 +41,7 @@ class Player:
 
 
 class Enemy:
+    # Creates a class that inherits from the main class. In this class, armor, health, and enemy damage are described
     def __init__(self, health, damage, armor):
         Person.__init__(self, health, damage, armor)
 
@@ -69,7 +72,7 @@ x = 1
 
 
 class Game:
-
+    # Makes the class of the game itself their attack on a friend of a friend, etc
     def ex_game():
         if player.ex_health() == True:
             return 'ENEMY WIN!'
@@ -100,7 +103,7 @@ game = Game
 
 gamewin = game.game()
 
-while gamewin == True:
+while gamewin == True: # Makes it all infinite to get 0 at the end
     gamewin = game.game()
 
 
